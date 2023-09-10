1adfff050a02898285297bfc749bfcf2f3b42a62
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-09-10 10:04:59 +0000

    [assignmen2] add-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,12 +19,41 @@ class BooksController < ApplicationController
     
     @book = Book.new
     
+    @books = Book.all
+    
   end
 
   def show
+    
+    @book = Book.find(params[:id])
+    
   end
 
   def edit
+    
+    @book = Book.find(params[:id])
+  
+    
+  end
+  
+  def update
+    
+    book = Book.find(params[:id])
+    
+    book.update(book_params)
+    
+    redirect_to book_path(book.id)
+    
+  end
+  
+  def destroy
+    
+    book = Book.find(params[:id])
+    
+    book.destroy
+    
+    redirect_to '/books'
+    
   end
 
 
